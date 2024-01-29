68691208e19d2ce3fcfba31995c6f94d1b54fef9
MarcoAguirre <viniciosoria16@gmail.com>
2024-01-28 23:41:20 -0500

    Add RapidAPI Key to make the API funcitonal

--- javascript/constants.js
+++ javascript/constants.js
@@ -3,7 +3,7 @@ export const API_MOVIE_ID_URL = 'https://moviesdatabase.p.rapidapi.com/titles/{i
 export const OPTIONS = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '<your api key>',
+		'X-RapidAPI-Key': 'e0d7ffac9bmsh96e1bd16c8f1e60p12a108jsn9e687988bfa1',
 		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
 	}
 };
